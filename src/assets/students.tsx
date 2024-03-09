42482ec0f61097375a4b5553d008b1ee17ad1e3d
unknown <konradmmo@gmail.com>
2024-03-09 20:59:19 +0100

    [HOTFIX] Remove duplicated student

--- src/assets/students.tsx
+++ src/assets/students.tsx
@@ -1,12 +1,6 @@
 const students = [
   {
     fullname: 'Konrad Rzodkiewicz',
-    studentId: '514222',
-    githubLink:
-      'https://github.com/LainonShiraya/51422_56698_55247_53379_54352/commits/main/?author=LainonShiraya',
-  },
-  {
-    fullname: 'Konrad Rzodkiewicz',
     studentId: '51422',
     githubLink:
       'https://github.com/LainonShiraya/51422_56698_55247_53379_54352/commits/main/?author=LainonShiraya',
