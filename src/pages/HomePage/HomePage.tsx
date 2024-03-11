5dbd28f1d7673a7ddc5caa718ff06f2baba31ee0
Konrad Rzodkiewicz <konradmmo@gmail.com>
2024-03-11 21:16:03 +0100

    [UI] Weekly Picked Mockup (#6)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import PageTemplate from '../PageTemplate/PageTemplate';
 import Banner from './Banner/Banner';
 import OptionPicker from './OptionPicker/OptionPicker';
+import WeeklyPicked from './WeeklyPicked/WeeklyPicked';
 
 const HomePage = () => {
   return (
     <PageTemplate>
       <Banner />
       <OptionPicker />
+      <WeeklyPicked />
     </PageTemplate>
   );
 };
