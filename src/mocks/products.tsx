cf2557c756f454c88420ab68c3d127e37409970d
adrianst89 <adrianst@vp.pl>
2024-05-04 08:57:55 +0200

    mocks

---
+++ src/mocks/products.tsx
@@ -0,0 +1,29 @@
+
+interface Product {
+    name: string;
+    id: number;
+    image: string;
+    }
+  
+  
+  const productArray: Product[] = [
+    { name: 'Switch', id: 1, image: 'switch.png' },
+    { name: 'Zamek Disneya', id: 2, image: 'zamek_disneya.png' },
+    { name: 'Vaiana i Merida', id: 3, image: 'vaiana_i_merida.png' },
+
+    { name: 'R2-D2™', id: 4, image: 'r2-d2.png' },
+    { name: 'Obi-Wan Kenobi™ i Darth Vader™', id: 5, image: 'wan_vader.png' },
+    { name: 'Duch i Upiór II', id: 6, image: 'duch_i_upiór.png' },
+    
+    { name: 'TIE Interceptor™', id: 7, image: 'tie_interceptor.png' },
+    { name: 'Powiększona minifigurka LEGO®', id: 8, image: 'powiekszona_miniaturka_lego.png' },
+    { name: 'Wieża Eiffla', id: 9, image: 'wieza_eiffla.png' },
+
+    { name: 'Duch i Upiór II', id: 10, image: 'duch_i_upiór.png' },
+    { name: 'Kreator mozaik', id: 10, image: 'kreator_mozaik.png' },
+    { name: 'Nowoczesna rezydencja Andrei', id: 10, image: 'nowoczesna_rezydencja_andrei.png' },
+    
+  ];
+  
+  export default productArray;
+  
